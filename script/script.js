0cfdcb56d2cd40df2bf9aa8bf2e2c546ea457f39
Andriy Sikora <Andriy.Sikora@gmail.com>
2016-07-03 19:10:37 +0300

    Add files to jumpbutton

--- script/script.js
+++ script/script.js
@@ -1,5 +1,14 @@
 var element = document.getElementById('box');
 
+window.onload = function(){
+	var w = window.outerWidth;
+    var h = window.outerHeight;
+    var randomX = Math.floor((Math.random() * (w-100)));
+    var randomY = Math.floor((Math.random() * (h-100)));
+    element.style.left = randomX+'px';
+    element.style.top = randomY+'px';
+}
+
 element.addEventListener('click', changePosition, true);
 
 function changePosition(){
@@ -7,8 +16,9 @@ function changePosition(){
     var h = window.outerHeight;
     var x = Math.floor((Math.random() * (w-100)));
     var y = Math.floor((Math.random() * (h-100)));
-    element.style.top = y+'px';
     element.style.left = x+'px';
-
-
+    element.style.top = y+'px';
 }
+
+
+
